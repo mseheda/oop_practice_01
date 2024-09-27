8353c16eb7d1a95c086105a4354548b3a4b30927
Mykhailo Seheda <147707994+mseheda@users.noreply.github.com>
2024-09-27 17:06:28 +0300

    refactoring

--- Healthcare-Hospital-Management-System/Healthcare-Hospital-Management-System/Program.cs
+++ Healthcare-Hospital-Management-System/Healthcare-Hospital-Management-System/Program.cs
@@ -1,6 +1,5 @@
-using System;
-using HealthcareHospitalManagementSystem.Models;
-using HealthcareHospitalManagementSystem.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace HealthcareHospitalManagementSystem
 {
@@ -9,29 +8,8 @@ namespace HealthcareHospitalManagementSystem
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            Doctor doctor = new Doctor
-            {
-                Name = "Dr. John Doe",
-                Age = 45,
-                Specialization = "Cardiology",
-                Department = "Cardiology"
-            };
-            
-            doctor.DisplayInfo();
-            
-            doctor.Work();
-            
-            IDoctorService doctorService = new DoctorService();
-            doctorService.AddDoctor(doctor);
-
-            Console.WriteLine("\nList of Doctors:");
-            foreach (var doc in doctorService.GetAllDoctors())
-            {
-                doc.DisplayInfo();
-            }
-            
-            Console.ReadLine();
         }
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
